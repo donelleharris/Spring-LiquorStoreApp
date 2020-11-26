2cc11f2b1963c1712b0e967efa2b4ec2d6fa9290
Donelle Harris <donelle.harris@gmail.com>
2020-11-25 21:02:13 -0600

    first commit

---
+++ src/main/java/com/me/liquorstoreap/LiquorStoreApApplication.java
@@ -0,0 +1,13 @@
+package com.me.liquorstoreap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquorStoreApApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiquorStoreApApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/me/liquorstoreap/LiquorStoreApApplicationTests.java
@@ -0,0 +1,13 @@
+package com.me.liquorstoreap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquorStoreApApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
