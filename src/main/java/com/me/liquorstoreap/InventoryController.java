6eeb7c728c2bf76362ad95841e894c5cdd98e130
Donelle Harris <donelle.harris@gmail.com>
2020-11-27 23:04:43 -0600

    Creating views and methods for CRUD functionality

--- src/main/java/com/me/liquorstoreap/InventoryController.java
+++ src/main/java/com/me/liquorstoreap/InventoryController.java
@@ -14,5 +14,31 @@ public class InventoryController {
     public String indexDisplay(){
         return "/index";
     }
+    @GetMapping("/addInventory")
+    public String addInventory(){
+        return "/addInventory";
+    }
+    @PostMapping("/addInventory")
+    public String addInventory(Model model){
+        return "/addInventory";
+    }
+    @GetMapping("/edit")
+    public String editInventory(){
+        return "/edit";
+    }
+    @PostMapping("/edit")
+    public String editInventory(Model model){
+        return "/edit";
+    }
+    
+//    add to edit page???
+//    @GetMapping("/delete")
+//    public String deleteInventory(){
+//        return "/delete";
+//    }
+//    @PostMapping("/delete")
+//    public String deleteInventory(Model model){
+//        return "/delete";
+//    }
 
 }
